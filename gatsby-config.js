673b81449a6e394c36755b3682fdb237abc717f9
David Berlin <47757213+davbree@users.noreply.github.com>
2024-03-10 17:58:00 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Sydney Ecommerce Theme`,
     siteUrl: `https://jamm.matter.design`,
-  }
+  },
   plugins: [
     {
       resolve: `gatsby-plugin-manifest`,
