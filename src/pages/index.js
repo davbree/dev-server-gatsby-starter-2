db114bb5a825b128a70bd8b0b032a2ec90ee9b25
David Berlin <47757213+davbree@users.noreply.github.com>
2024-03-14 09:12:21 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Main: Essentials for a cold winter!!!'}
+        title={'Main: Essentials for a cold winter!'}
         subtitle={'Discover Autumn Winter 2021'}
         ctaText={'shop now'}
         ctaAction={goToShop}
